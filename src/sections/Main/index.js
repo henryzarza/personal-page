595fce216cd41936afeca84290bee222500a1a85
Henry Zarza <henryacz7@gmail.com>
2020-08-15 16:19:35 -0500

    Main section

--- src/sections/Main/index.js
+++ src/sections/Main/index.js
@@ -1,28 +1,16 @@
 import React from 'react';
+import i18next from 'i18next';
 
 import styles from './styles.module.css';
 
 function Main() {
   return (
-    <div className='text-center'>
-      <h1 className='title'>
-        Almost before we knew it, we had left the ground.
-      </h1>
-      <p className='base-text'>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia
-        doloribus numquam minima at beatae. Error dolores maxime possimus vitae
-        adipisci, sed animi corrupti vero ab saepe repellendus, impedit,
-        deleniti voluptate.
-      </p>
-      <a
-        className={`base-text ${styles.link}`}
-        href='https://reactjs.org'
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        Learn React
-      </a>
-    </div>
+    <section className={styles.container}>
+      <header className={`text-center ${styles.header}`}>
+        <h1 className={`title ${styles.title}`}>{i18next.t('MAIN:NAME')}</h1>
+        <h3 className='small-title'>{i18next.t('MAIN:PROFESSION')}</h3>
+      </header>
+    </section>
   );
 }
 
