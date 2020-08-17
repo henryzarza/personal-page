a457c3d9dce78436fc28f56536e37c401ef673df
Henry Zarza <henryacz7@gmail.com>
2020-08-17 18:21:10 -0500

    Projects section

---
+++ src/sections/Projects/icons.js
@@ -0,0 +1,47 @@
+export const MOCKS = [
+  {
+    id: 1,
+    title: 'Graph Countries',
+    description: 'A React training app using a GraphQL API to query country-related',
+    image:
+      'https://d2slcw3kip6qmk.cloudfront.net/marketing/blog/2019Q1/project-planning/why-is-project-management-important-header@2x.png',
+    link: 'https://github.com/henryzarza/personal-page/pulls',
+    year: 2020
+  },
+  {
+    id: 2,
+    title: 'Tempo',
+    description: 'Landing page using React and between other things',
+    image:
+      'https://d2slcw3kip6qmk.cloudfront.net/marketing/blog/2019Q1/project-planning/why-is-project-management-important-header@2x.png',
+    link: 'https://github.com/henryzarza/personal-page/pulls',
+    year: 2020
+  },
+  {
+    id: 3,
+    title: 'PWA with React',
+    description: 'A React training app using a GraphQL API to query country-related',
+    image:
+      'https://d2slcw3kip6qmk.cloudfront.net/marketing/blog/2019Q1/project-planning/why-is-project-management-important-header@2x.png',
+    link: 'https://github.com/henryzarza/personal-page/pulls',
+    year: 2019
+  },
+  {
+    id: 4,
+    title: 'We Code',
+    description: 'A React training app using a GraphQL API to query country-related',
+    image:
+      'https://d2slcw3kip6qmk.cloudfront.net/marketing/blog/2019Q1/project-planning/why-is-project-management-important-header@2x.png',
+    link: 'https://github.com/henryzarza/personal-page/pulls',
+    year: 2019
+  },
+  {
+    id: 5,
+    title: 'React Marvel',
+    description: 'A React training app using a GraphQL API to query country-related',
+    image:
+      'https://d2slcw3kip6qmk.cloudfront.net/marketing/blog/2019Q1/project-planning/why-is-project-management-important-header@2x.png',
+    link: 'https://github.com/henryzarza/personal-page/pulls',
+    year: 2018
+  }
+];
---
+++ src/sections/Projects/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import i18next from 'i18next';
+
+import { MOCKS } from './icons';
+import WhiteCard from '../../components/WhiteCard';
+import styles from './styles.module.css';
+
+function Projects() {
+  return (
+    <section className={styles.container}>
+      <h2 className={`small-title ${styles.title}`}>{i18next.t('MAIN:PROJECTS_TITLE')}</h2>
+      <p className={`base-text ${styles.paragraph}`}>{i18next.t('MAIN:PROJECTS_DESCRIPTION')}</p>
+      <ul className={styles.timeline}>
+        <span className={styles.circle} />
+        {MOCKS.map((el) => (
+          <WhiteCard key={el.id} classContainer={styles.item} className={styles.card} data={el} />
+        ))}
+        <span className={styles.circle} />
+      </ul>
+    </section>
+  );
+}
+
+export default Projects;
