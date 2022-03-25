d69c3769d42360e3a98ca1a2f4a1cda772f91591
Henry Zarza <henryacz7@gmail.com>
2022-03-25 12:09:55 -0300

    Add i18n

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import '../i18n';
+import type { AppProps } from 'next/app';
 
 function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
