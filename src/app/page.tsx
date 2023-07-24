37808ac363ff9293fe4feed4362d25657df1079f
Henry Zarza <henryacz7@gmail.com>
2023-07-24 18:52:00 -0300

    Navbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,8 @@
-// import Image from 'next/image';
-import Link from 'next/link';
 import styles from './page.module.css';
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      {/* <Image
-        src="/vercel.svg"
-        alt="Vercel Logo"
-        className={styles.vercelLogo}
-        width={100}
-        height={24}
-        priority
-      /> */}
-      <Link href="/30-days-of-code">30 Days of Code</Link>
-      <Link href="/advent-of-css">Advent of CSS</Link>
       <h1 className='heading-1'>This is a Heading 1</h1>
       <h2 className='heading-2'>This is a Sub headig 1</h2>
       <h3 className='heading-3'>This is a Sub headig 3</h3>
@@ -22,10 +10,10 @@ export default function Home() {
       <p className='base-text'>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, tenetur. Veritatis incidunt libero repudiandae, architecto dolorum illum porro sint unde exercitationem aut inventore sed culpa accusamus minima, eaque repellendus corrupti!
       </p>
-      <p className='highlighted'>
+      <p className='highlighted-text'>
         Magnam, porro vitae, fuga provident ratione distinctio nihil maxime non saepe commodi, illo optio eum et dicta beatae ipsa sunt nesciunt veniam fugit ad tempora hic obcaecati veritatis. Cupiditate, soluta?
       </p>
-      <p className="small">
+      <p className="small-text">
         Hic corrupti velit suscipit a reiciendis animi quibusdam, recusandae molestias ipsum, eveniet porro officia ratione deserunt in vel commodi aliquid alias iste praesentium eum harum cumque dolore! Doloremque, eos quia.
       </p>
     </main>
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,5 +1,22 @@
+import Link from 'next/link';
+import styles from './styles.module.css';
+
 export default function Navbar() {
   return (
-    <nav>Navbar component</nav>
+    <nav className={styles.nav}>
+      <div className={styles['nav-content']}>
+        <div className={styles['nav-links']} aria-roledescription='List of internal links'>
+          <Link className={`highlighted-text ${styles.link} ${styles.active}`} href="#what-do-i-know">What do I know?</Link>
+          <Link className={`highlighted-text ${styles.link}`} href="#projects">Projects</Link>
+          <Link className={`highlighted-text ${styles.link}`} href="/advent-of-css">Advent of CSS</Link>
+          <Link className={`highlighted-text ${styles.link}`} href="/30-days-of-code">30DaysofCode</Link>
+          <Link className={`highlighted-text ${styles.link}`} href="#cookbook">Cookbook</Link>
+          <Link className={`highlighted-text ${styles.link}`} href="#inspiration">Inspiration</Link>
+        </div>
+        <button className={`highlighted-text ${styles['accordion-button']}`}>
+          Change Theme
+        </button>
+      </div>
+    </nav>
   )
 }
