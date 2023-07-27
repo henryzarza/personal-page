e8113139ed4a0e295cc01f35e23bf8f37bba296e
Henry Zarza <henryacz7@gmail.com>
2023-07-27 14:11:31 -0300

    Animation in WhatDoIknow section

--- src/app/home-sections/WhatDoIKnow/index.tsx
+++ src/app/home-sections/WhatDoIKnow/index.tsx
@@ -1,10 +1,39 @@
-import { WHAT_DO_I_KNOW_LIST } from '@/constants';
+'use client';
+
+import { useRef, useEffect } from 'react';
+
+import { WHAT_DO_I_KNOW_LIST, buildThresholdList } from '@/constants';
 import { ICONS } from './bgIcons';
 import styles from './styles.module.css';
 
 export default function WhatDoIKnow() {
+  const refInteraction = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const { current } = refInteraction;
+
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (current) {
+          const ratio = entry.intersectionRatio;
+          current.style.setProperty('--icons-movement-X', `${(ratio * 150).toFixed(2)}%`);
+          current.style.setProperty('--icons-movement-Y', `${(ratio * 200).toFixed(2)}%`);
+        }
+      },
+      { threshold: buildThresholdList(100) }
+    );
+
+    if (current) {
+      observer.observe(current);
+
+      return () => {
+        observer.unobserve(current);
+      };
+    }
+  }, []);
+
   return (
-    <section className={styles.container}>
+    <section className={styles.container} ref={refInteraction}>
       <div className={styles.content}>
         <h2 className="heading-2 white text-center">
           What do I know?
